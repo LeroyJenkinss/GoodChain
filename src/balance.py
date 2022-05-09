34cc74b811eef556b085c3082e41bc66c904b2d3
Tarik <thh.ualit@gmail.com>
2022-05-09 13:43:05 +0200

    balance without transactions possible

--- src/balance.py
+++ src/balance.py
@@ -5,19 +5,23 @@ class Balance:
 
     def calculateBalanceUntilTransaction(self, transId, minerId):
         try:
-            receivedValues = cur.execute("select sum(txvalue) from TRANSACTIONS T LEFT OUTER JOIN BLOCK B on T.poolid = B.poolid where (B.verifiedblock = 1 or T.poolid = 1) and T.reciever = (?) and T.Id < (?)",[minerId, transId]).fetchone()
+            receivedValues = cur.execute(
+                "select sum(txvalue) from TRANSACTIONS T LEFT OUTER JOIN BLOCK B on T.poolid = B.poolid where (B.verifiedblock = 1 or T.poolid = 1) and T.reciever = (?) and T.Id < (?)",
+                [minerId, transId]).fetchone()
             if receivedValues[0] is None:
                 receivedValues = 0
             else:
                 receivedValues = int(receivedValues[0])
 
-            sendValues = cur.execute("SELECT sum(txvalue) FROM TRANSACTIONS WHERE sender = (?) and id < (?)", [minerId, transId]).fetchone()
+            sendValues = cur.execute("SELECT sum(txvalue) FROM TRANSACTIONS WHERE sender = (?) and id < (?)",
+                                     [minerId, transId]).fetchone()
             if sendValues[0] is None:
                 sendValues = 0
             else:
                 sendValues = int(sendValues[0])
 
-            transValue = cur.execute("SELECT txvalue FROM TRANSACTIONS WHERE sender = (?) and id = (?)", [minerId, transId]).fetchone()
+            transValue = cur.execute("SELECT txvalue FROM TRANSACTIONS WHERE sender = (?) and id = (?)",
+                                     [minerId, transId]).fetchone()
             if transValue is None:
                 transValue = 0
             else:
@@ -32,5 +36,12 @@ class Balance:
     def returnBalance(self, userid):
         userRecieved = cur.execute("SELECT SUM(txvalue) FROM TRANSACTIONS WHERE reciever = (?)", [userid]).fetchone()[0]
         userSpend = cur.execute("SELECT SUM(txvalue) FROM TRANSACTIONS WHERE sender = (?)", [userid]).fetchone()[0]
-        # userSpendFee = cur.execute("SELECT SUM(txvalue) FROM TRANSACTIONS WHERE sender = (?)", [userid]).fetchone()[0]
-        return str(userRecieved - userSpend)
+        userSpendFee = cur.execute("SELECT SUM(txvalue) FROM TRANSACTIONS WHERE sender = (?)", [userid]).fetchone()[0]
+        if userRecieved is None:
+            userRecieved = 0
+        if userSpend is None:
+            userSpend = 0
+        if userSpendFee is None:
+            userSpendFee = 0
+
+        return str(userRecieved - userSpend - userSpendFee)
